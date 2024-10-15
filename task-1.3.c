84bfaa110625335802b90286d428662ea06841d3
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2024-10-15 22:28:07 +0300

    Update task-1.3.c

--- task-1.3.c
+++ task-1.3.c
@@ -1,13 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /*
 * @brief функция input принимает вводимые значение
 * @param number вводимое значение
 * @return возвращает вводимое значение
 */
-
 double input(void);
 
 /*
@@ -16,8 +16,14 @@ double input(void);
 * @param m масса ложки
 * @return возвращает значение изменения температуры
 */
-
 double get_t(const double Q, const double m);
+
+/*
+* @brief is Number проверяет меньше число нуля или нет
+* @param number число
+* @return возвращает true, если больше 0 и fasle, если меньше или равен 0
+*/
+bool isNumber(const double number);
 
 /*
 * @brief функция main выводит значение t, исходя из значений Q и m
@@ -25,20 +31,15 @@ double get_t(const double Q, const double m);
 * @param m масса ложки
 * @return возвращает 0 в случае успеха
 */
-
 int main() {
-  
-  double Q;
-
-  double m;
 
   puts("Введите значение Q:");
 
-  Q = input();
+  double Q = input();
 
   puts("Введите значение m:");
 
-  m = input();
+  double m = input();
 
   printf("t = %f\n", get_t(Q, m));
 }
@@ -47,7 +48,7 @@ double input(void) {
 
   double number;
 
-  if (scanf_s("%lf", &number) != 1 || number <= 0) {
+  if (scanf_s("%lf", &number) != 1 || !isNumber(number)) {
 
     puts("Значение не может быть меньше нуля");
 
@@ -63,4 +64,8 @@ double get_t(const double Q, const double m) {
   const double c = 462;
 
   return Q / (c * m);
+}
+
+bool isNumber(const double number){
+  return number > 0;
 }
