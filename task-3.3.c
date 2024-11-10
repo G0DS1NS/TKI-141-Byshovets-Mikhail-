e56f3536ce56863f3d7ac561e2f42ef8932b46b7
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2024-11-10 23:14:06 +0300

    Update task-3.3.c
    исправил ошибки

--- task-3.3.c
+++ task-3.3.c
@@ -1,78 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <float.h>
+#include <stdbool.h>
 
-/*
-* @brief input принимает на вход значение double, вводимое пользователем
-* @return возвращает значение, введеное пользователем
+
+/**
+* @brief id_positive проверяет, является ли h положительным числом
+* @param h параметр h
+* @return возвращает true, если h больше 0 и false в противном случае
 */
-double input(void);
+bool is_positive(const double h);
 
-/*
-* @bried get_answer выводин протабулированную функцию
-* @param x1 начало диапазона
-* param x2 конец диапазона
-* @param h параметр h, шаг
-*/
-void get_answer(const double h, double x1, const double x2);
-
-/*
-* @brief get_summ считает сумму функционального ряда
-* @param x параметр x
+/**
+* @brief get_next_element считает значение следующего элемента функционального ряда
 * @param n параметр n
-* @return возвращает значение суммы фукнуционального ряда
+* @param x параметр x
+* @return возвращает значение следующего элемента функционального ряда
 */
-double get_summ(const double x, const int n);
+double get_next_element(const int n, const double x, const double last_element);
 
 /**
-* @brief count_n_in_x_range
-* @param h параметр h
-* @return возвращает количество шагов h в диапазоне x
+* @brief finput принимает на вход значение типа double, ведённое пользователем
+* @return возвращает значение типа double, ведённое пользователем
 */
-int count_n_in_x_range(const double h);
+double finput(void);
 
-/**
-* @brief is_h проверяет значение h на правильность ввода
-* @param h параметр h
-* @return возращает true, если h больше 0 и false, если нет
-*/
-bool is_h(const double h);
+int main(void)
+{
 
-/*
-* @brief main принимает значение шага h и выводит аргумент функции, значение функции и сумму функионального ряда
-* @return возращает 0 в случае успеха
-*/
-int main(void) {
-	puts("Enter the x1:");
+	puts("Enter the start of arrea:");
 
-	double x1 = input();
+	double x1 = finput();
 
-	puts("Enter the x2:");
+	puts("Enter the end of arrea");
 
-	double x2 = input();
+	double x2 = finput();
 
-	puts("Enter the h:");
+	puts("Enter the step:");
 
-	double h = input();
+	double h = finput();
 
-	if (!is_h(h)) {
-
+	if (!is_positive(h))
+	{
 		puts("h is not positive");
 
 		return 1;
 	}
 
-	int n = 1;
+	double last_element = 1;
 
+	double start = x1;
+
+	double summ = 1;
 
 	printf("x\t\tcos(x)\t\tS\n");
 
+	int n = 1;
 
-	for (x1; x1 <= x2 + DBL_EPSILON; x1 += h) {
+	for (start; start <= x2 + DBL_EPSILON; start += h)
+	{
+		summ += get_next_element(n, start, last_element);
+		printf("%lf\t%lf\t%lf\n", start, cos(start), summ);
 
-		printf("%f\t%f\t%.4f\n", x1, cos(x1), get_summ(x1, n));
+		last_element = get_next_element(n, start, last_element);
 
 		n += 1;
 	}
@@ -80,13 +71,18 @@ int main(void) {
 	return 0;
 }
 
-double input(void) {
+double get_next_element(const int n, const double x, const double last_element) {
 
+	return last_element * -pow(x, 2) / (2 * n * (2 * n - 1));
+}
+
+double finput(void)
+{
 	double number = 0;
 
-	if (scanf_s("%lf", &number) != 1) {
-
-		puts("Your input is uncorrected");
+	if (scanf_s("%lf", &number) != 1)
+	{
+		puts("Yout input is uncorrected");
 
 		exit(EXIT_FAILURE);
 	}
@@ -94,38 +90,7 @@ double input(void) {
 	return number;
 }
 
-int count_n_in_x_range(const double h) {
-
-	int cnt = 0;
-
-	for (double x = 0.1; x <= 1; x += h) {
-
-		cnt += 1;
-	}
-
-	return cnt;
-}
-
-
-double get_summ(const double x, const int n) {
-
-	double summ = 0;
-
-	double func = 1 - pow(x, 2) / 2;
-
-	summ += func;
-
-	for (int i = 0; i <= n; i++) {
-
-		func *= pow(x, 2) / (4 * pow(n, 2) - 2 * n);
-
-		summ += func;
-	}
-
-	return summ;
-}
-
-bool is_h(const double h) {
-
-	return h > DBL_EPSILON;
+bool is_positive(const double h)
+{
+	return h > 0;
 }
